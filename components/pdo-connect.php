5021b4eee9f7caea4496597a9c82421d822a0a9e
Po Yu Chen <pyshif@gmail.com>
2021-12-07 11:59:12 +0800

    adjust pdo-connect.php var name

--- components/pdo-connect.php
+++ components/pdo-connect.php
@@ -1,16 +1,16 @@
 <?php
 
 session_start();
-$servername = "localhost";
-$username = "mfee222";
-$password = "222eefm";
-$dbname = "colorful";
+$colorful_servername = "localhost";
+$colorful_username = "mfee222";
+$colorful_password = "222eefm";
+$colorful_dbname = "colorful";
 
 try {
     $db_host = new PDO(
-        "mysql:host={$servername};dbname={$dbname};charset=utf8",
-        $username,
-        $password
+        "mysql:host={$colorful_servername};dbname={$colorful_dbname};charset=utf8",
+        $colorful_username,
+        $colorful_password
     );
     // echo "資料庫連線成功";
 } catch (PDOException $e) {
