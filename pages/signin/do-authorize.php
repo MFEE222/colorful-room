96e5b75f391a2ce489af56bde04753e616bdd595
Po Yu Chen <pyshif@gmail.com>
2021-12-08 21:50:17 +0800

    fix do-authorize path bug

--- pages/signin/do-authorize.php
+++ pages/signin/do-authorize.php
@@ -1,5 +1,11 @@
 <?php
-include_once('../var.php');
+// include_once('../var.php');
+if (str_contains($_SERVER['PHP_SELF'], 'index.php')) {
+    include_once('pages/var.php');
+} elseif (str_contains($_SERVER['PHP_SELF'], 'pages/')) {
+    include_once('../var.php');
+}
+
 session_start();
 // 檢查登入狀態
 if ($_SESSION['status'] === NULL || !$_SESSION['status']) {
