46fb462bc742a8cc84ce29c53d48c8ee5b39fe44
Po Yu Chen <pyshif@gmail.com>
2021-12-09 10:31:48 +0800

    fix breadcrumb

--- template/body-main-header.php
+++ template/body-main-header.php
@@ -4,11 +4,31 @@
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                    <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
-                </ol>
-                <h6 class="font-weight-bolder mb-0">Dashboard</h6>
+                <?php if (str_contains($_SERVER['PHP_SELF'], 'index.php')) : ?>
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Index</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Summary</li>
+                    </ol>
+                    <h6 class="font-weight-bolder mb-0">Summary</h6>
+                <?php elseif (str_contains($_SERVER['PHP_SELF'], 'pages/order')) : ?>
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Order</li>
+                    </ol>
+                    <h6 class="font-weight-bolder mb-0">Order</h6>
+                <?php elseif (str_contains($_SERVER['PHP_SELF'], 'pages/product')) : ?>
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Product</li>
+                    </ol>
+                    <h6 class="font-weight-bolder mb-0">Product</h6>
+                <?php elseif (str_contains($_SERVER['PHP_SELF'], 'pages/product')) : ?>
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Customer</li>
+                    </ol>
+                    <h6 class="font-weight-bolder mb-0">Customer</h6>
+                <?php endif; ?>
             </nav>
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                 <div class="ms-md-auto pe-md-3 d-flex align-items-center">
