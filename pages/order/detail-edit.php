3d4513c3212aea751c7d1e03d5dddcc5bda78891
Po Yu Chen <pyshif@gmail.com>
2021-12-12 12:47:57 +0800

    adjust detial-edit.php

--- pages/order/detail-edit.php
+++ pages/order/detail-edit.php
@@ -10,7 +10,14 @@ if (isset($_GET["id"])) {
 }
 require_once("../../components/pdo-connect.php");
 
-$sqlorder = "SELECT * FROM order_tracking WHERE id = $id";
+// $sqlorder = "SELECT * FROM order_tracking WHERE id = $id";
+
+$sqlorder = "SELECT *
+                FROM order_tracking
+                JOIN member ON order_tracking.user_id = member.id
+             WHERE order_tracking.id = ?";
+
+
 // "SELECT order_detail. *,tag.name AS tag_name FROM order_detail
 // JOIN tag ON order_detail.tag_id = tag.id
 // WHERE  order_detail.id=? AND order_detail.valid=1";
@@ -18,7 +25,8 @@ $sqlorder = "SELECT * FROM order_tracking WHERE id = $id";
 $stmt = $db_host->prepare($sqlorder);
 
 try {
-    $stmt->execute();
+    // $stmt->execute();
+    $stmt->execute([$id]);
     $rowsOrder = $stmt->fetchAll(PDO::FETCH_ASSOC);
     //    $rowsCountMember=$stmt->rowCount();
     //    var_dump($rowsOrder);
@@ -129,14 +137,14 @@ try {
                 <h3>訂單明細</h3>
             </div>
         </div>
-        <form action="detail-edit.php" method="post">
+        <form action="detail-update.php" method="post">
             <div class="row justify-content-end">
                 <div class="col-10">
                 </div>
                 <div class="col-2">
                     <a href="./order.php" class="demo">回列表</a>
                     <!-- <button class="demo" id="demo">Demo</button> -->
-                    <button type="button" class="demo" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <button type="submit" class="demo" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         送出</button>
                     <!-- <button><a href="./order.php" class="demo">取消</a></button> -->
                     <!-- <a href="./order.php" class="demo">回列表</a> -->
@@ -147,107 +155,109 @@ try {
             </div>
             <?php if ($rowsOrder == 0) : ?>
                 <p class="font-bold">使用者不存在</p>
-            <?php else :
-                $sqlorder = $stmt->fetch();
-            ?>
-            <div class="row">
-                <div class="col-9">
-                    <?php
-                    foreach ($rowsOrder as $value) :
-                    ?>
-                        <p>訂單編號 : <?= $value["order-num"] ?></p>
-                        <p>訂購日期 : <?= $value["date"] ?></p>
-                </div>
-            </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>商品品項</th>
-                        <th></th>
-                        <th>價格</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><?= $value["product-id"] ?></td>
-                        <td></td>
-                        <td>NT &#36 <?= $value["sum"] ?></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td class="text-right">商品小計</td>
-                        <td>NT &#36 <?= $value["sum"] ?></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td class="text-right">折扣小計</td>
-                        <td>NT &#36 <?= $value["discount"] ?></td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td></td>
-                        <td class="text-right">訂單總金額</td>
-                        <td>NT &#36 <?= $value["sum"] ?></td>
-                    </tr>
-                </tfoot>
-            </table>
+            <?php else : ?>
+                <?php foreach ($rowsOrder as $value) : ?>
+                    <div class="row">
+                        <div class="col-9">
+                            <p>訂單編號 : <?= $value["order_num"] ?></p>
+                            <p>訂購日期 : <?= $value["date"] ?></p>
+                        </div>
+                    </div>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>商品品項</th>
+                                <th></th>
+                                <th>價格</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $value["product_id"] ?></td>
+                                <td></td>
+                                <td>NT &#36 <?= $value["sum"] ?></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td class="text-right">商品小計</td>
+                                <td>NT &#36 <?= $value["sum"] ?></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td class="text-right">折扣小計</td>
+                                <td>NT &#36 <?= $value["discount"] ?></td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td></td>
+                                <td class="text-right">訂單總金額</td>
+                                <td>NT &#36 <?= $value["sum"] ?></td>
+                            </tr>
+                        </tfoot>
+                    </table>
 
-            <div class="row input-group-sm">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">付款</h5>
-                            <hr>
-                            <label for="payment_status" class="card-text form-label">付款方式 : </label><input type="text" class="form-control border border-secondary" id="payment_status" name="payment_status" value="<?= $value["payment_status"] ?>" required placeholder="請輸入付款方式">
-                            <label for="payment_method" class="card-text form-label">付款狀態 : </label><input type="text" class="form-control border border-secondary" id="payment_method" name="payment_method" value="<?= $value["payment_method"] ?>" required placeholder="請輸入付款狀態">
+                    <div class="row input-group-sm">
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">付款</h5>
+                                    <hr>
+                                    <label for="payment_status" class="card-text form-label d-block">
+                                        付款方式 :
+                                        <input type="text" class="form-control border border-secondary" id="payment_status" name="payment_status" value="<?= $value["payment_status"] ?>" required>
+                                    </label>
+                                    <label for="payment_method" class="card-text form-label d-block">
+                                        付款狀態 :
+                                        <input type="text" class="form-control border border-secondary" id="payment_method" name="payment_method" value="<?= $value["payment_method"] ?>" required placeholder="請輸入付款狀態">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">訂購人資訊</h5>
+                                    <hr>
+                                    <p class="card-text">顧客姓名 : <?= $value["name"] ?></p>
+                                    <p class="card-text">電話號碼 : <?= $value["phone"] ?></p>
+                                    <p class="card-text">電子郵件 : <?= $value["email"] ?></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">訂購人資訊</h5>
-                            <hr>
-                            <p class="card-text">顧客姓名 : <?= $value["user-id"] ?></p>
-                            <p class="card-text">電話號碼 : <?= $value["user-phone"] ?></p>
-                            <p class="card-text">電子郵件 : <?= $value["user-email"] ?></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">備註</h5>
-                            <hr>
-                            <p class="card-text">顧客備註 : <?= $value["remark"] ?></p>
-                            <label for="remark" class="card-text form-label">商家備註<input type="text" class="form-control border border-secondary card-text" id="remark" name="remark" value="<?= $value["remark"] ?>" required placeholder="請輸入備註">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">備註</h5>
+                                    <hr>
+                                    <p class="card-text">顧客備註 : <?= $value["remark"] ?></p>
+                                    <label for="remark" class="card-text form-label">商家備註<input type="text" class="form-control border border-secondary card-text" id="remark" name="remark" value="<?= $value["remark"] ?>" required placeholder="請輸入備註">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">發票</h5>
+                                    <hr>
+                                    <p class="card-text">電子發票</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">發票</h5>
-                            <hr>
-                            <p class="card-text">電子發票</p>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">訂單操作紀錄</h5>
+                                <hr>
+                                <p class="card-text text-danger"> <?= $value["record"] ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">訂單操作紀錄</h5>
-                        <hr>
-                        <p class="card-text text-danger"> <?= $value["record"] ?></p>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
+                <?php endforeach; ?>
         </form>
 
         <!-- Button trigger modal -->
@@ -271,36 +281,36 @@ try {
                 </div>
             </div>
         </div>
-        <?php endif; ?>
-                 
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script>
-            let form = document.getElementById("form"),
-                Btn = document.querySelector("#Btn"),
-                payment_status = document.querySelector("#payment_status"),
-                payment_method = document.querySelector("#payment_method"),
-                payment_statusError = document.querySelector("#payment_statusError"),
-                payment_methodError = document.querySelector("#payment_methodError");
-
-            btn.addEventListener("click", function(e) {
-                e.preventDefault();
-                let payment_statusExist = true;
-                if (payment_status.value === "") {
-                    payment_statusError.innerText = "狀態不能為空";
-                    // alert("帳號不能為空")
-                }
-                if (payment_method.value === "") {
-                    // alert("密碼不能為空")
-                    payment_methodError.innerText = "方式不能為空";
-                }
+    <?php endif; ?>
 
 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>
+        let form = document.getElementById("form"),
+            Btn = document.querySelector("#Btn"),
+            payment_status = document.querySelector("#payment_status"),
+            payment_method = document.querySelector("#payment_method"),
+            payment_statusError = document.querySelector("#payment_statusError"),
+            payment_methodError = document.querySelector("#payment_methodError");
 
-            });
-        </script>
+        btn.addEventListener("click", function(e) {
+            e.preventDefault();
+            let payment_statusExist = true;
+            if (payment_status.value === "") {
+                payment_statusError.innerText = "狀態不能為空";
+                // alert("帳號不能為空")
+            }
+            if (payment_method.value === "") {
+                // alert("密碼不能為空")
+                payment_methodError.innerText = "方式不能為空";
+            }
+
+
+
+        });
+    </script>
 
 
 </body>
